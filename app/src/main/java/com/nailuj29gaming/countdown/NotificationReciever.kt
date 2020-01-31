b0c6d6247d9e1daa5c10cdbd53ebbff9440872fc
Nailuj29Gaming <nailuj29gaming@gmail.com>
2020-01-30 19:19:22 -0500

    Initialize BroadcastReciever for daily notification
    Update 1.2 will feature a daily notification, preparing for this addition

---
+++ app/src/main/java/com/nailuj29gaming/countdown/NotificationReciever.kt
@@ -0,0 +1,13 @@
+package com.nailuj29gaming.countdown
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class NotificationReciever : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        // This method is called when the BroadcastReceiver is receiving an Intent broadcast.
+        TODO("NotificationReciever.onReceive() is not implemented")
+    }
+}
