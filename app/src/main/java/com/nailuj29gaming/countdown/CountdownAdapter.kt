c0fd1b8ff6e44bb22e9a57407014557001581784
Nailuj29Gaming <nailuj29gaming@gmail.com>
2020-02-01 09:45:15 -0500

    Started RecyclerView Adapter

---
+++ app/src/main/java/com/nailuj29gaming/countdown/CountdownAdapter.kt
@@ -0,0 +1,16 @@
+package com.nailuj29gaming.countdown
+
+import android.view.View
+import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.RecyclerView
+
+class CountdownAdapter : RecyclerView.Adapter<CountdownAdapter.ViewHolder> {
+
+
+
+    inner class ViewHolder(cardView: CardView) : RecyclerView.ViewHolder(cardView) {
+
+
+
+    }
+}
--- app/src/main/java/com/nailuj29gaming/countdown/CountdownDao.kt
+++ app/src/main/java/com/nailuj29gaming/countdown/CountdownDao.kt
@@ -1,6 +1,8 @@
 package com.nailuj29gaming.countdown
 
 import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
 import androidx.room.Query
 
 @Dao
@@ -9,8 +11,14 @@ interface CountdownDao {
     fun getAll(): List<Countdown>
 
     @Query("SELECT * FROM countdown WHERE id IN (:ids)")
-    fun getById(uids: IntArray): List<Countdown>
+    fun getById(ids: IntArray): List<Countdown>
 
     @Query("SELECT * FROM countdown ORDER BY date")
     fun sortByDates(): List<Countdown>
+
+    @Insert
+    fun insertAll(vararg countdowns: Countdown)
+
+    @Delete
+    fun delete(countdown: Countdown)
 }
